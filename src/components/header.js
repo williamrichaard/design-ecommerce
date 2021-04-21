1094eb7e478ca46e96a4dafd496352d5360a1d77
williamrichaard <wrichaard@hotmail.com>
2021-04-21 19:36:45 -0300

    Add Scroll Interactions

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+    render() {
+    return (
+      <div className="Header">
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
